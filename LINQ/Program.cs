1f1d3c8cf7346fc0ed98f7c7549ae8d50e5b7139
Jeremy Tucker <jeremytucker76@gmail.com>
2022-10-10 16:10:55 -0400

    Assignment complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace LINQ
-{
-    internal class Program
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQ
+{ 
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string> { "Sonic","Tetris", "Super Mario", "Super Smash Brothers" };
+
+            var GameNameLength = videoGames.OrderByDescending(game=>game.Length);
+
+            foreach( var game in GameNameLength)
+            {
+                Console.WriteLine(game);
+            }
+
+           /* var Length = from gameX in VideoGames
+                         orderby gameX.Length ascending
+                         select gameX;
+            foreach (string game in VideoGames)
+            {
+                Console.WriteLine("gameX");
+            }*/
+
         }
     }
 }
